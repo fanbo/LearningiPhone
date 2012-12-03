0601ab7d99b772296de9a713322566af587c697d
wellsnake <wellsunweizhen@gmail.com>
2012-12-03 21:09:19 +0800

    Initial Commit

---
+++ Psychologist/PsychologistAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PsychologistAppDelegate.h
+//  Psychologist
+//
+//  Created by Snake on 12-12-3.
+//  Copyright (c) 2012年 wellsnake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Psychologist/PsychologistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.h
+//  Psychologist
+//
+//  Created by Snake on 12-12-3.
+//  Copyright (c) 2012年 wellsnake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistViewController : UIViewController
+
+@end
